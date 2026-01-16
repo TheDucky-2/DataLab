7a0162d477d61e8b5339f98b84e2f4c7fc2aaf04
TheDucky-2 <datalab.project@protonmail.com>
2026-01-16 12:41:24 -0500

    Updated remove_currency_symbols() method to be polars based

--- datalab/tabular/data_cleaner/NumericalCleaner.py
+++ datalab/tabular/data_cleaner/NumericalCleaner.py
@@ -206,16 +206,34 @@ class NumericalCleaner(DataCleaner):
             NumericalCleaner(df).remove_currency_symbols()
         '''
         # ensuring that currency is detected at beginning or end of string
-        currency_in_start_or_end = r'^[\$\€\£\¥\₹\₩\₺\₫\₦\₱\₪\฿\₲\₴\₡]\s*\d[\d,]*(\.\d+)?$|^\d[\d,]*(\.\d+)?\s*[\$\€\£\¥\₹\₩\₺\₫\₦\₱\₪\฿\₲\₴\₡]$'
+        CURRENCY_SYMBOLS_PATTERN = r'^[$€£¥₹₩₺₫₦₱₪฿₲₴₡]\s*\d[\d,]*(.\d+)?$|^\d[\d,]*(.\d+)?s*[$€£¥₹₩₺₫₦₱₪฿₲₴₡]$'
 
+        CURRENCY_SYMBOLS = r'^[$€£¥₹₩₺₫₦₱₪฿₲₴₡]\s*|\s*[$€£¥₹₩₺₫₦₱₪฿₲₴₡]$'
+        
+        polars_df = BackendConverter(self.df[self.columns]).pandas_to_polars()
 
-        for col in self.df[self.columns]:
-            mask = self.df[col].astype(str).str.match(currency_in_start_or_end, na=False)
+        for col in polars_df.columns:
+            before = polars_df.select(pl.col(col)).to_series()
+            mask = polars_df.select(pl.col(col).str.contains(CURRENCY_SYMBOLS_PATTERN)).to_series()
             
-            # removing currency symbols
-            self.df.loc[mask, col] = self.df.loc[mask, col].astype(str).str.replace(r'[\$\€\£\¥\₹\₩\₺\₫\₦\₱\₪\฿\₲\₴\₡]', '', regex=True)
+            polars_df = polars_df.with_columns(
+                pl.when(pl.col(col).str.contains(CURRENCY_SYMBOLS_PATTERN))
+                .then(pl.col(col).str.replace_all(CURRENCY_SYMBOLS_PATTERN, ""))
+                .otherwise(pl.col(col))
+            )
+            
+            after = polars_df.select(pl.col(col)).to_series()
+        
+            self.track_not_cleaned(col = col, method= 'remove_currency_symbols', before = before, mask = mask, after = after)
 
-        return self.df
+        if self.inplace:
+            self.df[self.columns] = BackendConverter(polars_df).polars_to_pandas()
+            logger.info(f'Removed currency symbols in place.')
+            return None    
+        else:
+            df = BackendConverter(polars_df).polars_to_pandas()
+            logger.info(f'Removed currency symbols.')
+            return df
 
     def replace_commas_with_decimals(self)-> pd.DataFrame:
         '''
