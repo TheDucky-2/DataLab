c1ee2c5f37ca2cec71172b314170e0bc6c44e21d
TheDucky-2 <datalab.project@protonmail.com>
2026-01-23 15:18:47 +0530

    Updated formatting and docstrings

--- datalab/tabular/data_loader/loader.py
+++ datalab/tabular/data_loader/loader.py
@@ -1,22 +1,29 @@
+"""Loads pandas DataFrame from a tabular dataset"""
+
 import pandas as pd
 from pathlib import Path
 import polars as pl
 
-def load_tabular(file_path: str, file_type: str = None, array_type='auto', conversion_threshold:int=None, **kwargs) -> pd.DataFrame:
-    '''
+def load_tabular(file_path: str, file_type: str = None, array_type: str='auto', conversion_threshold:int=None, **kwargs:dict) -> pd.DataFrame:
+    """
     Use this function for loading your tabular data as a pandas DataFrame.
-    
-    Parameters:
+
+    Parameters
     ------------
 
     file_path: str
         Path of your data file or just file name
   
-    file_type: str (default is 'csv')
-        Supported File Types: 'csv', 'excel, 'parquet', 'JSON'
+    file_type: str, optional
+        Supported File Types:
+        
+        - 'csv' (default)
+        - 'excel'
+        - 'parquet'
+        - 'JSON'
 
-    array_type: str (default is 'auto')
-        Determines the array/backend type used in pandas operations.
+    array_type: str 
+        Determines the array/backend type used in pandas operations, by default 'auto'.
 
         Options are:
 
@@ -24,33 +31,34 @@ def load_tabular(file_path: str, file_type: str = None, array_type='auto', conve
         - 'pyarrow' -> PyArrow backend for better performance on large datasets
         - 'auto' -> automatically selects backend based on input and dataset size 
             
-    conversion_threshold: int (default is 100000)
-        The number of rows at which the conversion from Polars to pandas switches to Arrow-backed pandas arrays for performance. 
+    conversion_threshold: int
+        The number of rows at which the conversion from Polars to pandas switches to Arrow-backed pandas arrays for performance, default is 100000.
         Users can increase or decrease this threshold depending on their dataset size and memory availability.
        
     kwargs: dict
-        Extra arguments you want to pass into pandas file readers (for excel and JSON files only).
+        Extra arguments you want to pass into pandas file readers (for excel files only).
      
-    Returns:
+    Returns
     ---------
-        pd.DataFrame (a pandas DataFrame)
+    pd.DataFrame
+        A pandas DataFrame
     
-    Usage Recommendation:
+    Usage Recommendation
     ---------------------
         Use this function to load datasets quickly without memorizing multiple read functions.
         Polars -> pandas conversion ensures efficient memory usage and stability, even on low-RAM systems.
 
-    Considerations:
+    Considerations
     ---------------
         Adjust array_type and conversion_threshold for very large datasets to optimize performance and memory usage.
 
-    Example:
-    --------------
+    Example
+    --------
     >>> # Load a CSV file (default parameters)
         df1 = load_tabular('example.csv')
 
     >>> # Load an Excel file
-        df2 = load_tabular('example.xlsx', file_type='excel')               # file_type is optional
+        df2 = load_tabular('example.xlsx', file_type='excel')             
 
     >>> # Load a Parquet file using PyArrow backend
         df3 = load_tabular('example.parquet', array_type='pyarrow')
@@ -59,8 +67,8 @@ def load_tabular(file_path: str, file_type: str = None, array_type='auto', conve
         df4 = load_tabular('large_dataset.csv', conversion_threshold=2000000)
 
     >>> # Load a JSON file from a subdirectory with auto array backend
-        df5 = load_tabular('some/path/to/data.json') 
-    '''
+        df5 = load_tabular('some/path/to/data.json')
+    """
     if not isinstance(file_path, (str, Path)):
         raise TypeError('file path must be a string or a file path')
 
@@ -105,4 +113,3 @@ def load_tabular(file_path: str, file_type: str = None, array_type='auto', conve
 
     elif array_type == 'pyarrow':
         return polars_df.to_pandas(use_pyarrow_extension_array=True)
-    
