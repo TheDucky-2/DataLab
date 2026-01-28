f83183decee8aa091e407c72cd6a72dfb9668a18
TheDucky-2 <datalab.project@protonmail.com>
2026-01-28 22:35:36 +0530

    Updated tests

--- tests/import tests/test_imports.py
+++ tests/import tests/test_imports.py
@@ -1,5 +1,6 @@
 ## Basic tests to check whether datalab modules are importing.
-def testing_datalab_imports():
+def test_datalab_imports():
+    import pytest
 
     modules = ['datalab',
                'datalab.tabular.data_loader',
@@ -8,15 +9,17 @@ def testing_datalab_imports():
                'datalab.tabular.data_preprocessor',
                'datalab.tabular.data_visualization',
                'datalab.tabular.computations',
-               'datalab.tabular.utils'] 
+               'datalab.tabular.utils']
+
+    modules_not_found = []
     
     for module in modules:
         try:
             __import__(module)
 
         except ModuleNotFoundError as error:
-            print(f'Module not found {module}: {error}')
-            raise error
+            modules_not_found.append(module)
 
-    print("Imported datalab modules successfully!")
+    if modules_not_found:
+        pytest.fail(f'The following modules could not be imported: {", ".join(modules_not_found)}')
 
